4904207e8f4c0663a4813d6e6c92c009dc87addf
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-11-02 11:58:28 -0400

    GUI Initial

--- Java/src/Binary.java
+++ Java/src/Binary.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Binary {
+    public static Scanner myObj = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Would you like to convert from Decimal to Binary (A) or Binary to Decimal (B)? \n");
+
+        String choice = myObj.nextLine();
+        if(choice.equals("A")) {
+            decimalIn();
+        }
+        if(choice.equals("B")) {
+            binaryIn();
+        }
+    }
+    private static void decimalIn() {
+        System.out.println("What number would you like to convert? \n");
+
+        int decimalInput = myObj.nextInt();
+        System.out.print(Integer.toBinaryString(decimalInput));
+    }
+
+    private static void binaryIn() {
+        System.out.println("What number would you like to convert? \n");
+
+        int binaryInput = myObj.nextInt();
+        System.out.println(Integer.parseInt(String.valueOf(binaryInput),2));
     }
 }
---
+++ Python/test.java
@@ -0,0 +1,15 @@
+int x; 
+int y;
+
+if (x > 10 || y = 5) {
+    x = y*4;
+}
+y = x/4
+x = x + 2
+if (x = 10 || y > 20) {
+    z = x * y
+}
+else {
+    y = x
+    z = x + y
+}
