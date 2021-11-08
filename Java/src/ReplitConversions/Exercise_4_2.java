7afcc609bd3bc15c1bc3f68decab75f1fb06d5f5
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-11-08 07:52:55 -0500

    Exercise 4.2

---
+++ Java/src/ReplitConversions/Exercise_4_2.java
@@ -0,0 +1,17 @@
+package ReplitConversions;
+
+public class Exercise_4_2 {
+    public static void main(String[] args) {
+        // Griffin Byrd, 11/8/2021, Project Stem 4.2 Exercise A
+        System.out.println("This is not my first real program!");
+        /* Java is a very cool programming language.
+        And this is a multi line comment.
+         */
+
+        // \n Sends text to a new line. Example:
+        System.out.println("This is the first line. \nThis is the next line.");
+        // \t Is like indenting - it is the same thing as hitting tab.
+        System.out.println("\t See?");
+
+    }
+}
---
+++ Java/src/ReplitConversions/README.md
@@ -0,0 +1,4 @@
+# AP-CSP 2021-2022
+
+## Replit Conversions
+Replit Python to Java conversions for all the work I need to do.
