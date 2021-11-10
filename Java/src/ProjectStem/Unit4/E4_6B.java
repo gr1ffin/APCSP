86cb52133992d0043d0f79fda7dfaade4c65c6a0
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-11-10 08:40:38 -0500

    4.6 B updates
    now checks for correct/wrong spots

--- Java/src/ProjectStem/Unit4/E4_6B.java
+++ Java/src/ProjectStem/Unit4/E4_6B.java
@@ -8,30 +8,51 @@ public class E4_6B {
     public static ArrayList<Integer> randomNumbers = new ArrayList<>();
     private static int guessAmount = 0;
     public static void main(String[] args) {
-        Random rand = new Random();
         for (int a = 0; a < 4; a++) {
-            randomNumbers.add(rand.nextInt(9));
+            randomGen();
         }
-        System.out.println("What do you think the four numbers are?");
+
+        System.out.println("What do you think the four numbers are? (0-9)");
+        System.out.println("Note - there are no repeating numbers!");
         Scanner guessInput = new Scanner(System.in);
         while (true) {
-            int z = 0;
+            int wrongSpot = 0;
+            int correctSpot = 0;
             for (int b = 0; b < 4; b++) {
                 int guessNumber = Integer.parseInt(guessInput.nextLine());
+                for (int c = 0; c < 4; c++) {
+                    if (guessNumber == randomNumbers.get(c)) {
+                        wrongSpot += 1;
+                    }
+                }
                 if (guessNumber == randomNumbers.get(b)) {
-                    z += 1;
+                    correctSpot += 1;
                 }
             }
+            if (wrongSpot > 4) {
+                System.out.println("Error");
+            }
             guessAmount += 1;
-            if (z == 4) {
+            if (correctSpot == 4) {
                 System.out.println("Hooray! You got all 4 numbers correct! \nThe computer's numbers were " + randomNumbers + "\nIt took you " + guessAmount + " guesses.");
                 break;
             }
             else{
-                System.out.println("Out of all your guesses, you got " + z + " correct.");
+                System.out.println("You had " + correctSpot + " numbers in the correct spot. \nYou had " + wrongSpot + " correct numbers but in the wrong spot. ");
             }
 
             System.out.println("Try Again!");
+        }
+    }
+
+    public static void randomGen() {
+        Random rand = new Random();
+        int newRandom = rand.nextInt(9);
+        if (randomNumbers.contains(newRandom)) {
+            randomGen();
+        }
+        else {
+            randomNumbers.add(newRandom);
         }
     }
 }
