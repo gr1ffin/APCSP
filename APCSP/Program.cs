ae1bd34f04a5de3f1d70eb8e73b10f9e6204db26
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-12-10 08:57:05 -0500

    c#

---
+++ APCSP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace APCSP
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            
+            int i = 0;
+            int[] randomList = new int[50000];
+            while (i < 50000)
+            {
+                Random random = new Random();
+
+                int n = random.Next(100000, 999999);
+                randomList.Append(n);
+                i++;
+            }
+            Array.Sort(randomList);
+            BinarySearch(randomList, 9772240);
+        }
+
+        static void BinarySearch(int[] arr, int target)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+            int mid = 0;
+            int count = 0;
+            while (low <= high)
+            {
+                count = count + 1;
+                mid = (high + low) / 2;
+                if (arr[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                else if (arr[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    Console.WriteLine("Found, " + count);
+                }
+            }
+
+        }
+    }
+}
