9b52a054ceda602232b68f91865b1d19f69b72bb
gr1ffin <89878211+gr1ffin@users.noreply.github.com>
2021-12-14 08:22:53 -0500

    random stuff

---
+++ DaphneCounter/DaphneCounter/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System.Collections.Generic;   //Need this to use lists<>.
+
+
+namespace GoogleSheetsAndCSharp
+{
+    class Program
+    {
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "Counter";
+        static readonly string SpreadsheetID = "1zaVfRInZZnz0oml589kXT-j7OFI2LSn5iCq5A1VdKn4";
+        static readonly string sheet = "Sheet1";
+        static SheetsService service;
+        public static int ExistingData;
+        public static int NewData;
+
+        public static void Start()
+        {
+            Console.WriteLine("Application Started!");
+
+            GoogleCredential credential;
+            using (var stream = new FileStream("C:\\Users\\agent\\OneDrive\\Documents\\GitHub\\AP-CSP\\counterconsole\\sheets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            ReadEntries();
+        }
+
+        static void ReadEntries()
+        {
+            var range = $"{sheet}!A1:F10";
+            var request = service.Spreadsheets.Values.Get(SpreadsheetID, range);
+
+            var response = request.Execute();
+            var values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    Console.WriteLine("{0}", row[1]);
+                    var test = row[1];
+                    ExistingData = int.Parse(s: test.ToString());
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+        }
+
+        static void CreateEntry()
+        {
+            var range = $"{sheet}!B2";
+            var valueRange = new ValueRange();
+
+            var objectList = new List<object>() { Convert.ToString(4) };
+            valueRange.Values = new List<IList<object>> { objectList };
+
+            var appendRequest = service.Spreadsheets.Values.Append(valueRange, SpreadsheetID, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            var appendResponse = appendRequest.Execute();
+        }
+
+        static void UpdateEntry(string updateText)
+        {
+            var range = $"{sheet}!A2";
+            var valueRange = new ValueRange();
+            var objectList = new List<object>() { updateText };
+            valueRange.Values = new List<IList<object>> { objectList };
+
+            var updateRequest = service.Spreadsheets.Values.Update(valueRange, SpreadsheetID, range);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;   //UpdateRequest in this line, is different from the variable updateRequest
+            var updateResponse = updateRequest.Execute();
+        }
+
+        public static void ButtonPress()
+        {
+            ReadEntries();
+            NewData = ExistingData + 1;
+            UpdateEntry(Convert.ToString(NewData));
+        }
+
+    }
+}
